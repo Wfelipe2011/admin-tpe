8be122ad2459601287535f92ce4c1a669c365479
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-05-31 23:16:39 -0300

    refactor: remove overflow hidden from body, adjust header layout, and add new images

--- components/public/designation-header.tsx
+++ components/public/designation-header.tsx
@@ -4,16 +4,16 @@ export function DesignationHeader() {
   return (
     <div className="relative">
       <Image
-        src="/images/ellipse.png"
+        src="/ellipse.png"
         alt="ellipse"
         width={500}
         height={300}
         className="w-full -z-10 -mt-11 md:hidden"
       />
 
-      <div className="flex flex-col justify-center items-center -mt-28 gap-4 text-md font-semibold">
+      <div className="flex flex-col justify-center items-center -mt-24 gap-4 text-md font-semibold">
         <div className="text-gray-50">Bem-vindo(a) ao TPE DIGITAL</div>
-        <Image src="/images/logo.png" alt="logo" width={200} height={100} className="w-1/2" />
+        <Image src="/logo.png" alt="logo" width={100} height={80} />
       </div>
     </div>
   )
